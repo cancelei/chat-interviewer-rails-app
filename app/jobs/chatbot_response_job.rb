9e012c2569cf6c39efb883169f2ebba1e032a93d
cancelei <glauber.bannwart@gmail.com>
2024-06-08 16:26:03 -0300

    update error handling for responses

--- app/jobs/chatbot_response_job.rb
+++ app/jobs/chatbot_response_job.rb
@@ -1,17 +1,19 @@
-# app/jobs/chatbot_response_job.rb
 class ChatbotResponseJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    openai_service = OpenAiService.new
-    response = openai_service.get_response(message.content)
+    if message.is_a?(Message) # Ensure the message is of the expected type
+      openai_service = OpenAiService.new
+      response = openai_service.get_response(message.content)
 
-    if response.present?
-      message.update(response: response)
-      # Broadcast the response to the chat channel
-      ActionCable.server.broadcast "chat_channel", message: render_message(message)
+      if response.present?
+        message.update(response: response)
+        ActionCable.server.broadcast "chat_channel", message: render_message(message)
+      else
+        Rails.logger.error "Failed to get a response from OpenAI"
+      end
     else
-      Rails.logger.error "Failed to get a response from OpenAI"
+      Rails.logger.error "Expected message to be an instance of Message, got #{message.class}"
     end
   end
 
