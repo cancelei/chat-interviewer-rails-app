f9d8adbccc0bdebfbd70f09e910199db6504c946
cancelei <glauber.bannwart@gmail.com>
2024-06-09 17:14:13 -0300

    Add status column to applicants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_183546) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_201318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_183546) do
     t.string "interview_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
