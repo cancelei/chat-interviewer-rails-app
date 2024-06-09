f8d9e710fef54bd90ac9660006e98462197aac5d
cancelei <glauber.bannwart@gmail.com>
2024-06-09 17:43:47 -0300

    Improve Job decision flow (still flaky)

--- app/jobs/chatbot_response_job.rb
+++ app/jobs/chatbot_response_job.rb
@@ -15,23 +15,27 @@ class ChatbotResponseJob < ApplicationJob
     applicant = Applicant.last || Applicant.create(status: 0)
     openai_service = OpenAiService.new
 
-    if message.include?("não") || message.include?("no") || applicant.status == 1
+    if applicant.status == 0  # If status is 0, use OpenAI to respond
+      if message.downcase.include?("não") || message.downcase.include?("no")
+        applicant.update(status: 1)  # Change status if the user says "no"
+        continue
+      end
       response = openai_service.get_response(message)
       Rails.logger.info "Chatbot response: #{response}"
       ActionCable.server.broadcast("chat_channel", { message: render_message(response) })
-      applicant.update(status: 1)
-    else
-      response = openai_service.get_response(message)
-      Rails.logger.info "Chatbot response: #{response}"
-      ActionCable.server.broadcast("chat_channel", { message: render_message(response) })
-    end
-
-    # Ask the next question
-    next_question_index = applicant.responses.count
-    if next_question_index < QUESTIONS.length
-      next_question = QUESTIONS[next_question_index]
-      applicant.responses.create(content: message)  # Store the user's message
-      ActionCable.server.broadcast("chat_channel", { message: render_message(next_question) })
+      if message.downcase.include?("não") || message.downcase.include?("no")
+        applicant.update(status: 1)  # Change status if the user says "no"
+      end
+    else  # If status is 1, ask the next preformatted question
+      next_question_index = applicant.responses.count
+      if next_question_index < QUESTIONS.length
+        next_question = QUESTIONS[next_question_index]
+        applicant.responses.create(content: message)  # Store the user's message
+        ActionCable.server.broadcast("chat_channel", { message: render_message(next_question) })
+      else
+        # Reset status to 0 for any further OpenAI related responses
+        applicant.update(status: 0)
+      end
     end
   end
 
