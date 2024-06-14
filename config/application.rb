755793a96d8ac5a277471a093475c1a7c8931a98
cancelei <glauber.bannwart@gmail.com>
2024-06-14 10:07:19 -0300

    chore: Remove Dotenv::Rails.load from application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Rails.load
-
 module AppNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
